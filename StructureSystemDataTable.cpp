755df9a4feb85cc8f7fbc9910bb3b0410f6b6879
martin1194 <martinc1194@gmail.com>
2021-08-24 07:20:01 +0800

    Add files via upload

---
+++ StructureSystemDataTable.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "../StructureSystem/StructureSystemDataTable.h"
+
+
+static ConstructorHelpers::FObjectFinder<UDataTable> StrutureList(TEXT("DataTable'/Game/StarterContent/NewDataTable.NewDataTable'"));
+
+void UStructureSystemDataTable::addtolist()
+{
+	FStructureSystemData addtolist;
+	addtolist.name = "HI";
+	StrutureList->AddRow(FName(TEXT("Texting")), addtolist);
+
+	UE_LOG(LogTemp, Warning, TEXT("StructureSystem/UStructureSystemDataTable::BeginPlay()"));
+}
---
+++ StructureSystemDataTable.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "StructureSystemDataTable.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FStructureSystemData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Table)
+		FString name;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Table)
+		FText description;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Table)
+		int32 skill_required;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Table)
+		int32 skill_required2;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class VILLAGESIMULATOR_API UStructureSystemDataTable : public UDataTable
+{
+	GENERATED_BODY()
+
+
+public:
+	void addtolist();
+
+	UPROPERTY()
+		class UDataTable* StrutureList;
+};
